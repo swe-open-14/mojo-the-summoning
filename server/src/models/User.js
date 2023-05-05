83f71532f0381ba61867db03a40267c688826642
Todi Olubode <todimu18@gmail.com>
2023-05-05 10:34:44 +0100

    yo

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,20 @@
-// create your User model here
+const User = sequelize.define('User', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
